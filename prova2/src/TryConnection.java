5120baca19605f713c165e227e675923abe79b31
luca9294 <luca9294@hotmail.it>
2013-05-08 14:10:59 +0200

    Connected to the UNI-server, called some queries in JAVA

--- prova2/src/TryConnection.java
+++ prova2/src/TryConnection.java
@@ -1,50 +1,27 @@
-import java.sql.DriverManager;
-import java.sql.Connection;
-import java.sql.SQLException;
+
+import java.sql.*;
  
 public class TryConnection {
  
-	public static void main(String[] argv) {
- 
-		System.out.println("-------- PostgreSQL "
-				+ "JDBC Connection Testing ------------");
- 
-		try {
- 
-			Class.forName("org.postgresql.Driver");
- 
-		} catch (ClassNotFoundException e) {
- 
-			System.out.println("Where is your PostgreSQL JDBC Driver? "
-					+ "Include in your library path!");
-			e.printStackTrace();
-			return;
- 
-		}
- 
-		System.out.println("PostgreSQL JDBC Driver Registered!");
- 
-		Connection connection = null;
- 
-		try {
- 
-			connection = DriverManager.getConnection(
+	public static void main(String[] argv) throws SQLException {
+			Connection connection = DriverManager.getConnection(
 					"jdbc:postgresql://10.7.20.170:5432/postgres", "ds_group3",
 					"Phie5pia");
+			
+			Statement stmt = connection.createStatement();
+            ResultSet rs;
  
-		} catch (SQLException e) {
+            rs = stmt.executeQuery("SELECT * FROM point");
+            
+            while ( rs.next() ) {
+                String point_id = rs.getString("point_id");
+                String point_name = rs.getString("point_name");
+                String elevation = rs.getString("elevation");
+                
+                System.out.println();
+                System.out.print(point_id + " ");
+                System.out.print(point_name + " ");
+                System.out.print(elevation +  " ");
  
-			System.out.println("Connection Failed! Check output console");
-			e.printStackTrace();
-			return;
- 
-		}
- 
-		if (connection != null) {
-			System.out.println("You made it, take control your database now!");
-		} else {
-			System.out.println("Failed to make connection!");
-		}
-	}
- 
-}
+
+}}}
